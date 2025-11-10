0c89c2e87035bd779dd2728213c02552baa9e3bb
PandoraMiracle <sakurahanna896@gmail.com>
2025-11-10 21:42:23 +0700

    Add stock components

---
+++ client/app/stock/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { StockPage } from "@/components/StockPage";
+import { MainLayout } from "@/components/MainLayout";
+
+export default function StockPageRoute() {
+    return (
+        <MainLayout>
+            <StockPage />
+        </MainLayout>
+    );
+}
+
