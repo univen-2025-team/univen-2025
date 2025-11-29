ee9ad2553e7ce8d873d6aa9f2e5ae691c4cbdb52
tvconss <tranvanconkg@gmail.com>
2025-11-29 17:04:23 +0700

    fix: conflict

--- server/src/configs/user.config.ts
+++ server/src/configs/user.config.ts
@@ -4,17 +4,10 @@ export const USER_PUBLIC_FIELDS: (keyof model.auth.UserSchema)[] = [
     'user_avatar',
     'user_fullName',
     'user_role',
-<<<<<<< HEAD
-    "user_gender",
-    "user_status",
-    "user_dayOfBirth",
-    "balance",
-=======
     'user_gender',
     'user_status',
     'user_dayOfBirth',
     'balance'
->>>>>>> ea92ef4d712de077556c73c19678cf028ca8fded
 ];
 
-export const USER_INIT_BALANCE = 10_000;
+export const USER_INIT_BALANCE = 20_000_000;
