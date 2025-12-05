0b04310ed3511c03c0226f661c18cefb39568255
tvconss <tranvanconkg@gmail.com>
2025-12-05 21:25:36 +0700

    fix: update client url

--- client-new/config/app.ts
+++ client-new/config/app.ts
@@ -1,2 +1,2 @@
 export const APP_URL = 'https://univen-2025-6cmweeh57-tranvanconkgs-projects.vercel.app';
-export const API_URL = 'http://ec2-3-25-106-203.ap-southeast-2.compute.amazonaws.com:4000/v1/api';
+export const API_URL = 'https://ec2-3-25-106-203.ap-southeast-2.compute.amazonaws.com/v1/api';
