39780e406720d1e35f509c11cf1b7a6d23da57d0
tvconss <tranvanconkg@gmail.com>
2025-12-05 18:08:31 +0700

    remove: client

--- client/app/dashboard/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { MainLayout } from '@/components/MainLayout';
-
-export default function DashboardLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return <MainLayout>{children}</MainLayout>;
-}
--- client/lib/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from './store';
-
-// Sử dụng trong toàn bộ ứng dụng thay vì `useDispatch` và `useSelector` thông thường
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useSelector.withTypes<RootState>();
