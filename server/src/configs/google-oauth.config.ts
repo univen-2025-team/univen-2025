73ade02d606cf260e58853e7d41d8ca78d78bd15
tvconss <tranvanconkg@gmail.com>
2026-01-28 07:18:00 +0700

    cicd

--- server/src/configs/google-oauth.config.ts
+++ server/src/configs/google-oauth.config.ts
@@ -1,6 +1,6 @@
 export const GOOGLE_OAUTH_CLIENT_ID = process.env.GOOGLE_OAUTH_CLIENT_ID || '';
 export const GOOGLE_OAUTH_CLIENT_SECRET = process.env.GOOGLE_OAUTH_CLIENT_SECRET || '';
-export const GOOGLE_OAUTH_REDIRECT_URI = process.env.GOOGLE_OAUTH_REDIRECT_URI;
+export const GOOGLE_OAUTH_REDIRECT_URI = process.env.GOOGLE_OAUTH_REDIRECT_URI || 'https://univen-1111-api.duckdns.org/v1/api/auth/login/google/callback';
 
 // if (!GOOGLE_OAUTH_CLIENT_ID || !GOOGLE_OAUTH_CLIENT_SECRET) {
 //     throw new Error('Google OAuth credentials are not set in environment variables.');
